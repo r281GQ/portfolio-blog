e3600740df565878f90e92cb10a577b48acc6947
Endre Vegh <endre.vegh@yahoo.com>
2018-03-11 19:35:59 +0000

    create pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,37 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+
+const path = require('path');
+
+exports.createPages = async ({
+  boundActionCreators: { createPage },
+  graphql
+}) => {
+  const BlogPost = path.resolve('./src/components/BlogPost/index.jsx');
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            id
+            frontmatter {
+              path
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: BlogPost
+    });
+  });
+};
