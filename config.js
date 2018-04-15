1b0a48cadb4de581f1c365ed6082badefa76b5e1
Endre Vegh <endre.vegh@yahoo.com>
2018-04-15 12:01:42 +0100

    create basic config.js

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  twitter_url: `https://twitter.com/endre_vegh`
+  siteFBAppID: '',
+  userTwitter: '@endre_vegh',
+  twitter_url: `https://twitter.com/endre_vegh`,
+  siteUrl: 'http://localhost:8000/',
+  siteTitle: '',
+  siteDescription: '',
+  siteLogo: '',
+  pathPrefix: '/'
 };
