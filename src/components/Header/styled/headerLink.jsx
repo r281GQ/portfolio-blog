30f09e752efb318bcd15d696180ae8960591c043
Endre Vegh <endre.vegh@yahoo.com>
2018-04-16 06:51:52 +0100

    refactor header

--- src/components/Header/styled/headerLink.jsx
+++ src/components/Header/styled/headerLink.jsx
@@ -2,5 +2,9 @@ import styled from 'styled-components';
 import Link from '../../Shared/Link';
 
 export default styled(Link)`
-  color: ${({ active }) => (active ? `#D1C4E9` : `#B388FF`)};
+  color: ${({ active, theme: { primaryColor, secondaryColor } }) =>
+    !active ? secondaryColor : primaryColor};
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-size: 0.6rem;
 `;
--- src/components/Header/styled/linkFlexContainer.jsx
+++ src/components/Header/styled/linkFlexContainer.jsx
@@ -1,8 +1,3 @@
 import styled from 'styled-components';
 
-export default styled.div`
-  position: absolute;
-  ${props => (props.alignment === 'right' ? `right: 0px` : `left: 0px`)};
-  top: 0px;
-  height: 100%;
-`;
+export default styled.div``;
